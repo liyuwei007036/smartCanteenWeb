3a1a312afb839be14ebe54d6e900a8c3c05fd32a
lc <599047945@qq.com>
2020-03-20 00:11:24 +0800

    添加默认值

--- src/config/global.js
+++ src/config/global.js
@@ -1,8 +1,8 @@
 // const HTTP_URL = 'http://47.114.176.72:81/smj/'
 // const SOCKET_URL = 'ws://47.114.176.72:81/ws/'
 
-const HTTP_URL = 'http://127.0.0.1:5001'
-const SOCKET_URL = 'ws://127.0.0.1:8032/ws/'
+const HTTP_URL = 'http://exus.51mypc.cn:5001'
+const SOCKET_URL = 'ws://nexus.51mypc.cn:8032/ws/'
 export {
     HTTP_URL,
     SOCKET_URL,
