9899fb7b1da1c13cd259117f9528437379892c0a
lc <599047945@qq.com>
2020-03-20 00:17:49 +0800

    添加默认值

--- src/config/global.js
+++ src/config/global.js
@@ -1,7 +1,7 @@
 // const HTTP_URL = 'http://47.114.176.72:81/smj/'
 // const SOCKET_URL = 'ws://47.114.176.72:81/ws/'
 
-const HTTP_URL = 'http://exus.51mypc.cn:5001'
+const HTTP_URL = 'http://exus.51mypc.cn:5002'
 const SOCKET_URL = 'ws://nexus.51mypc.cn:8032/ws/'
 export {
     HTTP_URL,
