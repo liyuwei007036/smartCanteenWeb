9f93b01b0bcca9a7db6ac37c73bbe0b1e765d475
lc <599047945@qq.com>
2020-03-20 23:36:27 +0800

    添加多环节打包

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,7 +1,7 @@
 'use strict'
-const merge = require('webpack-merge')
-const prodEnv = require('./prod.env')
-
-module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
-})
+module.exports = {
+    NODE_ENV: 'develop',
+    EVN_CONFIG: 'dev',
+    HTTP_URL: 'http://192.168.3.100:5001/',
+    SOCKET_URL: 'ws://192.168.3.100:8032/ws/'
+}
--- config/test.env.js
+++ config/test.env.js
@@ -3,5 +3,8 @@ const merge = require('webpack-merge')
 const devEnv = require('./dev.env')
 
 module.exports = merge(devEnv, {
-  NODE_ENV: '"testing"'
+    NODE_ENV: '"testing"',
+    EVN_CONFIG: 'test',
+    HTTP_URL: 'http://192.168.3.100:5001/',
+    SOCKET_URL: 'ws://192.168.3.100:8032/ws/'
 })
