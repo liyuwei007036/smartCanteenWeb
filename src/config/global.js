20c64e6eafd4989968209e96fbbf501e48246b96
lc <599047945@qq.com>
2020-03-20 20:36:19 +0800

    修改错误

--- src/config/global.js
+++ src/config/global.js
@@ -1,8 +1,8 @@
 // const HTTP_URL = 'http://47.114.176.72:81/smj/'
 // const SOCKET_URL = 'ws://47.114.176.72:81/ws/'
 
-const HTTP_URL = 'http:/:5001/'
-const SOCKET_URL = 'ws:/:8032/ws/'
+const HTTP_URL = 'http://192.168.3.100:5001/'
+const SOCKET_URL = 'ws://192.168.3.100:8032/ws/'
 export {
     HTTP_URL,
     SOCKET_URL,
