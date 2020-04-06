b4fed47f61f221e5d2b03ff60da4c8575f1f577c
lc <599047945@qq.com>
2020-04-06 10:02:42 +0800

    修改样式

--- src/App.vue
+++ src/App.vue
@@ -14,11 +14,11 @@
                 window.oncontextmenu = function () {
                     return false;
                 }
-                //禁止任何键盘敲击事件（防止F12和shift+ctrl+i调起开发者工具）
-                window.onkeydown = window.onkeyup = window.onkeypress = function () {
-                    window.event.returnValue = false;
-                    return false;
-                }
+                // //禁止任何键盘敲击事件（防止F12和shift+ctrl+i调起开发者工具）
+                // window.onkeydown = window.onkeyup = window.onkeypress = function () {
+                //     window.event.returnValue = false;
+                //     return false;
+                // }
             }
         },
         provide() {
